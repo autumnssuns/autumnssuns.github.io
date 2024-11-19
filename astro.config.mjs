ec2c302b9947e5d11fdc035f20eab17cff51b2eb
Dan Tran <d.k.tran@uq.edu.au>
2024-11-19 16:10:25 +1000

    Added path alias to astro config for vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import { path } from 'path';
 
 import icon from 'astro-icon';
 
@@ -10,4 +11,12 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
   integrations: [tailwind(), icon(), mdx(), svelte()],
   site: 'https://autumnssuns.github.io',
+  vite: {
+    resolve: {
+      alias: {
+        '@layouts': path.resolve(path.dirname(''), 'src/layouts'),
+        '@blogs': path.resolve(path.dirname(''), 'src/pages/blogs'),
+      },
+    },
+  },
 });
